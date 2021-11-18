60ec499abcdfac3ba97b9fcf806d76498b96aad2
Alestry Pérez <alestry.perez@hyperisland.se>
2021-11-18 14:52:21 +0100

    REST end point created

---
+++ index.js
@@ -0,0 +1,32 @@
+import Express from 'express';
+import Products from './products.js';
+
+const app = Express();
+const port = 3000;
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+const mid = (req, res, next) => {
+  console.log(req.query);
+  console.log(req.params);
+  next();
+};
+
+// GET, PUT, POST, DELETE
+
+app.get('/products/:id', mid, (req, res) => {
+  res.json(
+    Products.find((product) => {
+      return +req.params.id === product.id;
+    })
+  );
+  // res.send(req.params.id);
+  // res.json(Products);
+});
+
+app.post('/add', (req, res) => {
+  console.log(req.body.id);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => console.log('port listening!' + port));
---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: 1,
+    name: 'Soap',
+  },
+  {
+    id: 2,
+    name: 'Tooth Brush',
+  },
+  {
+    id: 3,
+    name: 'Shoe',
+  },
+  {
+    id: 4,
+    name: 'Chips',
+  },
+];
+
+export default products;
